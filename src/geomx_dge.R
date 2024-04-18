eee1f0b60beeea9bb1e34352ce53662217f5c8da
Niemiec Iga <iganiemi@lx2-304-27010.ad.helsinki.fi>
2024-04-18 15:43:18 +0300

    jaccard idx + hclust pathways clustering which works awesome :D

--- src/geomx_dge.R
+++ src/geomx_dge.R
@@ -24,6 +24,8 @@ library(GSVA)
 library(ggpubr)
 library(topGO)
 library(fgsea)
+library(fpc)
+library(dbscan)
 
 
 # get variables -----------------------------------------------------------
@@ -47,60 +49,65 @@ source('/media/iganiemi/T7-iga/st/geomx-processing/src/geomx_utils.R')
 
 geomx_obj <- readRDS(input_rds_path)
 
-# # make DGE between selected ROI groups ------------------------------------
-# 
-# # within slide analysis - with random slope in LLM
-# # comparison between ++ (posCD8_posIBA1) and other groups
-# 
-# # convert test variables to factors
-# for(col in c(imp_vars, 'Sample')){
-#   pData(geomx_obj)[[paste0(col, "_factor")]] <- factor(pData(geomx_obj)[[col]])
-# }
-# 
-# # convert normalized counts to log scale
-# assayDataElement(object = geomx_obj, elt = paste0("log_", norm_type)) <-
-#   assayDataApply(geomx_obj, 2, FUN = log, base = 2, elt = norm_type)
-# 
-# # run LMM:
-# # formula follows conventions defined by the lme4 package
-# results <- c()
-# for(segment in c("tumor", "stroma")){
-#   # careful! this is from all table with umap made for all ROIs - should be change to avoid confusion
-#   geomx_segment <- geomx_obj[, geomx_obj@phenoData@data$Segment == segment]
-#   for(status in c("pre", "post")) {
-#     ind <- geomx_segment@phenoData@data$`NACT status` == status
-# 
-#     mixedOutmc <-
-#       mixedModelDE(geomx_segment[, ind],
-#                    elt = "log_q3_norm",
-#                    modelFormula = ~ Annotation_cell_factor + (1 + Annotation_cell_factor | Sample_factor), # random slope
-#                    groupVar = "Annotation_cell_factor",
-#                    nCores = (parallel::detectCores() - 1),
-#                    multiCore = FALSE)
-# 
-# 
-#     # format results as data.frame
-#     r_test <- do.call(rbind, mixedOutmc["lsmeans", ])
-#     tests <- rownames(r_test)
-#     r_test <- as.data.frame(r_test)
-#     r_test$Contrast <- tests
-# 
-#     # use lapply in case you have multiple levels of your test factor to
-#     # correctly associate gene name with it's row in the results table
-#     r_test$Gene <-
-#       unlist(lapply(colnames(mixedOutmc),
-#                     rep, nrow(mixedOutmc["lsmeans", ][[1]])))
-#     r_test$Subset <- status
-#     r_test$Segment <- segment
-#     r_test$FDR <- p.adjust(r_test$`Pr(>|t|)`, method = "fdr")
-#     r_test <- r_test[, c("Gene", "Subset", "Segment",  "Contrast", "Estimate",
-#                          "Pr(>|t|)", "FDR")]
-#     results <- rbind(results, r_test)
-#   }
-# }
-# 
-# fwrite(results, file.path(output_dir, 'dge/dge_annotation_cell_pre_post_separately.csv'))
-# 
+# fix patients PFS
+geomx_obj@phenoData@data$PFS <- ifelse(geomx_obj@phenoData@data$Patient %in% c('S027', 'S065'), 'Long',
+                                       ifelse(geomx_obj@phenoData@data$Patient == 'S139', 'Short', 
+                                              geomx_obj@phenoData@data$PFS))
+
+# make DGE between selected ROI groups ------------------------------------
+
+# within slide analysis - with random slope in LLM
+# comparison between ++ (posCD8_posIBA1) and other groups
+
+# convert test variables to factors
+for(col in c(imp_vars, 'Sample')){
+  pData(geomx_obj)[[paste0(col, "_factor")]] <- factor(pData(geomx_obj)[[col]])
+}
+
+# convert normalized counts to log scale
+assayDataElement(object = geomx_obj, elt = paste0("log_", norm_type)) <-
+  assayDataApply(geomx_obj, 2, FUN = log, base = 2, elt = norm_type)
+
+# run LMM:
+# formula follows conventions defined by the lme4 package
+results <- c()
+for(segment in c("tumor", "stroma")){
+  # careful! this is from all table with umap made for all ROIs - should be change to avoid confusion
+  geomx_segment <- geomx_obj[, geomx_obj@phenoData@data$Segment == segment]
+  for(status in c("pre", "post")) {
+    ind <- geomx_segment@phenoData@data$`NACT status` == status
+
+    mixedOutmc <-
+      mixedModelDE(geomx_segment[, ind],
+                   elt = "log_q3_norm",
+                   modelFormula = ~ Annotation_cell_factor + (1 + Annotation_cell_factor | Sample_factor), # random slope
+                   groupVar = "Annotation_cell_factor",
+                   nCores = (parallel::detectCores() - 1),
+                   multiCore = FALSE)
+
+
+    # format results as data.frame
+    r_test <- do.call(rbind, mixedOutmc["lsmeans", ])
+    tests <- rownames(r_test)
+    r_test <- as.data.frame(r_test)
+    r_test$Contrast <- tests
+
+    # use lapply in case you have multiple levels of your test factor to
+    # correctly associate gene name with it's row in the results table
+    r_test$Gene <-
+      unlist(lapply(colnames(mixedOutmc),
+                    rep, nrow(mixedOutmc["lsmeans", ][[1]])))
+    r_test$Subset <- status
+    r_test$Segment <- segment
+    r_test$FDR <- p.adjust(r_test$`Pr(>|t|)`, method = "fdr")
+    r_test <- r_test[, c("Gene", "Subset", "Segment",  "Contrast", "Estimate",
+                         "Pr(>|t|)", "FDR")]
+    results <- rbind(results, r_test)
+  }
+}
+
+fwrite(results, file.path(output_dir, 'dge/dge_annotation_cell_pre_post_separately.csv'))
+
 # # without differentiation to pre and post
 # 
 # results2 <- c()
@@ -233,47 +240,45 @@ geomx_obj <- readRDS(input_rds_path)
 # fwrite(results_pre_post_doublepos_signif, file.path(output_dir, 'dge/dge_pre_post_doublepos_signif.csv'))
 # 
 # ################################
-# # for alltogether post samples long vs short pfs
-# geomx_post <- geomx_obj[, geomx_obj@phenoData@data$`NACT status` == 'post']
-# 
-# results_pfs_doublepos <- c()
-# for(segment in c("tumor", "stroma")){
-#   # careful! this is from all table with umap made for all ROIs - should be change to avoid confusion
-#   geomx_segment <- geomx_post[, geomx_post@phenoData@data$Segment == segment]
-#   geomx_segment_doublepos <- geomx_segment[, geomx_segment@phenoData@data$Annotation_cell == "posCD8_posIBA1"]
-# 
-#   mixedOutmc <-
-#     mixedModelDE(geomx_segment_doublepos,
-#                  elt = "log_q3_norm",
-#                  modelFormula = ~ PFS_factor + (1 | Sample_factor), # random slope
-#                  groupVar = "PFS_factor",
-#                  nCores = (parallel::detectCores() - 1),
-#                  multiCore = FALSE)
-# 
-# 
-#   # format results as data.frame
-#   r_test <- do.call(rbind, mixedOutmc["lsmeans", ])
-#   tests <- rownames(r_test)
-#   r_test <- as.data.frame(r_test)
-#   r_test$Contrast <- tests
-# 
-#   # use lapply in case you have multiple levels of your test factor to
-#   # correctly associate gene name with it's row in the results table
-#   r_test$Gene <-
-#     unlist(lapply(colnames(mixedOutmc),
-#                   rep, nrow(mixedOutmc["lsmeans", ][[1]])))
-#   r_test$Segment <- segment
-#   r_test$FDR <- p.adjust(r_test$`Pr(>|t|)`, method = "fdr")
-#   r_test <- r_test[, c("Gene", "Segment",  "Contrast", "Estimate",
-#                        "Pr(>|t|)", "FDR")]
-#   results_pfs_doublepos <- rbind(results_pre_post_doublepos, r_test)
-# }
-# 
-# fwrite(results_pfs_doublepos, file.path(output_dir, 'dge/dge_pfs_doublepos.csv'))
-# results_pfs_doublepos_signif <- results_pfs_doublepos[results_pfs_doublepos$FDR <= 0.05, ]
-# fwrite(results_pfs_doublepos_signif, file.path(output_dir, 'dge/dge_pfs_doublepos_signif.csv'))
-# 
-# 
+# for alltogether post samples long vs short pfs
+geomx_post <- geomx_obj[, geomx_obj@phenoData@data$`NACT status` == 'post']
+
+results_pfs_doublepos <- c()
+for(segment in c("tumor", "stroma")){
+  # careful! this is from all table with umap made for all ROIs - should be change to avoid confusion
+  geomx_segment <- geomx_post[, geomx_post@phenoData@data$Segment == segment]
+  geomx_segment_doublepos <- geomx_segment[, geomx_segment@phenoData@data$Annotation_cell == "posCD8_posIBA1"]
+
+  mixedOutmc <-
+    mixedModelDE(geomx_segment_doublepos,
+                 elt = "log_q3_norm",
+                 modelFormula = ~ PFS_factor + (1 | Sample_factor), # random slope
+                 groupVar = "PFS_factor",
+                 nCores = (parallel::detectCores() - 1), # parallel doesnt work :/
+                 multiCore = FALSE)
+
+
+  # format results as data.frame
+  r_test <- do.call(rbind, mixedOutmc["lsmeans", ])
+  tests <- rownames(r_test)
+  r_test <- as.data.frame(r_test)
+  r_test$Contrast <- tests
+
+  # use lapply in case you have multiple levels of your test factor to
+  # correctly associate gene name with it's row in the results table
+  r_test$Gene <-
+    unlist(lapply(colnames(mixedOutmc),
+                  rep, nrow(mixedOutmc["lsmeans", ][[1]])))
+  r_test$Segment <- segment
+  r_test$FDR <- p.adjust(r_test$`Pr(>|t|)`, method = "fdr")
+  r_test <- r_test[, c("Gene", "Segment",  "Contrast", "Estimate",
+                       "Pr(>|t|)", "FDR")]
+  results_pfs_doublepos <- rbind(results_pfs_doublepos, r_test)
+}
+
+fwrite(results_pfs_doublepos, '/media/iganiemi/T7-iga/st/geomx-processing/results/nact/dge/old/dge_pfs_doublepos_updated.csv')
+
+
 # #############################################################
 # #############################################################
 # # separately per each patient pre and post, all
@@ -367,70 +372,70 @@ geomx_obj <- readRDS(input_rds_path)
 
 
 
-# make DGE between selected ROI groups ------------------------------------
-
-# within slide analysis - with random slope in LLM
-# comparison between ++ (posCD8_posIBA1) and other groups
-
-# convert test variables to factors
-for(col in c(imp_vars, 'Sample')){
-  pData(geomx_obj)[[paste0(col, "_factor")]] <- factor(pData(geomx_obj)[[col]])
-}
-
-# convert normalized counts to log scale
-assayDataElement(object = geomx_obj, elt = paste0("log_", norm_type)) <-
-  assayDataApply(geomx_obj, 2, FUN = log, base = 2, elt = norm_type)
-
-# run LMM:
-# formula follows conventions defined by the lme4 package
-results <- c()
-for(segment in c("tumor", "stroma")){
-  # careful! this is from all table with umap made for all ROIs - should be change to avoid confusion
-  geomx_segment <- geomx_obj[, geomx_obj@phenoData@data$Segment == segment]
-  for(status in c("pre", "post")) {
-    ind <- geomx_segment@phenoData@data$`NACT status` == status
-
-    mixedOutmc <-
-      mixedModelDE(geomx_segment[, ind],
-                   elt = "log_q3_norm",
-                   modelFormula = ~ Annotation_cell_factor + (1 + Annotation_cell_factor | Sample_factor), # random slope
-                   groupVar = "Annotation_cell_factor",
-                   nCores = (parallel::detectCores() - 1),
-                   multiCore = FALSE)
-
-
-    # format results as data.frame
-    r_test <- do.call(rbind, mixedOutmc["lsmeans", ])
-    tests <- rownames(r_test)
-    r_test <- as.data.frame(r_test)
-    r_test$Contrast <- tests
-
-    # use lapply in case you have multiple levels of your test factor to
-    # correctly associate gene name with it's row in the results table
-    r_test$Gene <-
-      unlist(lapply(colnames(mixedOutmc),
-                    rep, nrow(mixedOutmc["lsmeans", ][[1]])))
-    r_test$Subset <- status
-    r_test$Segment <- segment
-    r_test$FDR <- p.adjust(r_test$`Pr(>|t|)`, method = "fdr")
-    r_test <- r_test[, c("Gene", "Subset", "Segment",  "Contrast", "Estimate",
-                         "Pr(>|t|)", "FDR")]
-    results <- rbind(results, r_test)
-  }
-}
-
-fwrite(results, file.path(output_dir, 'dge/dge_annotation_cell_pre_post_separately.csv'))
+# # make DGE between selected ROI groups ------------------------------------
+# 
+# # within slide analysis - with random slope in LLM
+# # comparison between ++ (posCD8_posIBA1) and other groups
+# 
+# # convert test variables to factors
+# for(col in c(imp_vars, 'Sample')){
+#   pData(geomx_obj)[[paste0(col, "_factor")]] <- factor(pData(geomx_obj)[[col]])
+# }
+# 
+# # convert normalized counts to log scale
+# assayDataElement(object = geomx_obj, elt = paste0("log_", norm_type)) <-
+#   assayDataApply(geomx_obj, 2, FUN = log, base = 2, elt = norm_type)
+# 
+# # run LMM:
+# # formula follows conventions defined by the lme4 package
+# results <- c()
+# for(segment in c("tumor", "stroma")){
+#   # careful! this is from all table with umap made for all ROIs - should be change to avoid confusion
+#   geomx_segment <- geomx_obj[, geomx_obj@phenoData@data$Segment == segment]
+#   for(status in c("pre", "post")) {
+#     ind <- geomx_segment@phenoData@data$`NACT status` == status
+# 
+#     mixedOutmc <-
+#       mixedModelDE(geomx_segment[, ind],
+#                    elt = "log_q3_norm",
+#                    modelFormula = ~ Annotation_cell_factor + (1 + Annotation_cell_factor | Sample_factor), # random slope
+#                    groupVar = "Annotation_cell_factor",
+#                    nCores = (parallel::detectCores() - 1),
+#                    multiCore = FALSE)
+# 
+# 
+#     # format results as data.frame
+#     r_test <- do.call(rbind, mixedOutmc["lsmeans", ])
+#     tests <- rownames(r_test)
+#     r_test <- as.data.frame(r_test)
+#     r_test$Contrast <- tests
+# 
+#     # use lapply in case you have multiple levels of your test factor to
+#     # correctly associate gene name with it's row in the results table
+#     r_test$Gene <-
+#       unlist(lapply(colnames(mixedOutmc),
+#                     rep, nrow(mixedOutmc["lsmeans", ][[1]])))
+#     r_test$Subset <- status
+#     r_test$Segment <- segment
+#     r_test$FDR <- p.adjust(r_test$`Pr(>|t|)`, method = "fdr")
+#     r_test <- r_test[, c("Gene", "Subset", "Segment",  "Contrast", "Estimate",
+#                          "Pr(>|t|)", "FDR")]
+#     results <- rbind(results, r_test)
+#   }
+# }
+# 
+# fwrite(results, file.path(output_dir, 'dge/dge_annotation_cell_pre_post_separately.csv'))
 
 
 # enrichment on DGE -------------------------------------------------------
-# TODO examine FDR calculation on the lvl of DGE
+
 dge_data_dir <- file.path("/media/iganiemi/T7-iga/st/geomx-processing/results/nact/dge")
 
-#list.files(dge_data_dir)
+list.files(file.path(dge_data_dir, 'old'))
 
-#dge_df <- fread(file.path(dge_data_dir, "dge_annotation_cell_all.csv"))
 dge_df <- fread(file.path(dge_data_dir, "old",  "dge_annotation_cell_pre_post_separately.csv"))
 #dge_df <- fread(file.path(dge_data_dir, "dge_patient_pairs_doublepos.csv"))
+dge_df <- fread(file.path(dge_data_dir, "old",  "dge_pfs_doublepos_updated.csv"))
 
 # select thr
 fc_thr <- 1
@@ -510,14 +515,15 @@ msigdb_res <- data.frame(msigdb_res@result) %>%
 
 rank_type <- 'rank_fdr_fcval' # c('rank_p', 'rank_p_fcval', 'rank_fdr_fcval')
 
+#dge_df <- filter(dge_df, Subset == 'post') #for anno comparison
+
 gsea_all <- lapply(unique(dge_df$Contrast), function(contrast){
   lapply(unique(dge_df$Segment), function(segment){
     
-    dge_all_sub <- filter(dge_df, Subset == 'post' &
-                            Contrast == contrast &
+    dge_all_sub <- filter(dge_df, Contrast == contrast &
                             Segment == segment)
     
-    # TODO why is it different than the one saved from DGE calculation??
+    # different from DGE calculation cause fdr was for all segments and contrasts. this is appropriate one
     dge_all_sub$FDR2 <- p.adjust(dge_all_sub$`Pr(>|t|)`, method = "fdr") 
     
      dge_all_sub$rank_p <- sign(dge_all_sub$Estimate)*(-log10(dge_all_sub$`Pr(>|t|)`))
@@ -575,22 +581,121 @@ gsea_all <- lapply(unique(dge_df$Contrast), function(contrast){
 
 gsea_all <- do.call(rbind, unlist(gsea_all, recursive=FALSE))
 
-fwrite(gsea_all, file.path(dge_data_dir, 'gsea', paste0('gsea_post_', rank_type, '.csv')))
+fwrite(gsea_all, file.path(dge_data_dir, 'gsea', paste0('gsea_post_pfi_', rank_type, '.csv')))
 
 
 # inspect GSEA results ----------------------------------------------------
 
-gsea_res_path <- "/media/iganiemi/T7-iga/st/geomx-processing/results/nact/dge/gsea/gsea_post_rank_p_fcval.csv"
+gsea_res_path <- "/media/iganiemi/T7-iga/st/geomx-processing/results/nact/dge/gsea/gsea_post_rank_fdr_fcval.csv"
 
 gsea_res <- fread(gsea_res_path)
 
 # filter only for doublepositive comparisons
 gsea_res <- gsea_res[grepl('posCD8_posIBA1', gsea_res$Contrast), ]
-gsea_up <- gsea_res[gsea_res$NES > 0, ]
-gsea_down <- gsea_res[gsea_res$NES < 0, ]
 
-length(unique(gsea_up$pathway))
-sort(table(gsea_up$pathway), decreasing = T)
+###
+gsea_up <- gsea_res[gsea_res$NES > 0, ] # downregulated in ++
+gsea_down <- gsea_res[gsea_res$NES < 0, ] # upregulated in ++
 
-gsea_up_stroma <- gsea_up[gsea_up$Segment == 'stroma', ]
-gsea_up_tumor <- gsea_up[gsea_up$Segment == 'tumor', ]
+length(unique(gsea_down$pathway))
+sort(table(gsea_down$pathway), decreasing = T)
+
+gsea_down_stroma <- gsea_down[gsea_down$Segment == 'stroma', ]
+gsea_down_tumor <- gsea_down[gsea_down$Segment == 'tumor', ]
+
+length(unique(gsea_down_stroma$pathway))
+path_nr <- as.data.frame(sort(table(gsea_down_stroma$pathway), decreasing = T))
+
+# analyse which pathways are upregulated in 3 and 2 
+sharing <- sapply(unique(gsea_down_stroma$pathway), function(x){
+  gsea_p <- gsea_down_stroma$Contrast[gsea_down_stroma$pathway == x]
+  
+  if(length(gsea_p) == 3){
+    y <- 'shared_3'
+  } else if(length(gsea_p) == 2){
+    if(!("negCD8_negIBA1 - posCD8_posIBA1" %in% gsea_p)){
+      y <- 'shared_2_no_doubleneg'
+    } else if(!("negCD8_posIBA1 - posCD8_posIBA1" %in% gsea_p)){
+      y <- 'shared_2_no_negpos'
+    } else if(!("posCD8_negIBA1 - posCD8_posIBA1" %in% gsea_p)){
+      y <- 'shared_2_no_negpos'
+    }
+  } else{
+    y <- paste0('shared_1_', gsea_p)
+  }
+})
+
+sharing_pathway <- data.frame(pathway = unique(gsea_down_stroma$pathway), sharing = sharing)
+
+#################################
+# for short-long PFI
+
+gsea_res_path <- "/media/iganiemi/T7-iga/st/geomx-processing/results/nact/dge/gsea/gsea_post_pfi_rank_fdr_fcval.csv"
+
+gsea_res <- fread(gsea_res_path)
+
+gsea_down <- gsea_res[gsea_res$NES < 0, ] # upregulated in short
+gsea_down_stroma <- gsea_down[gsea_down$Segment == 'stroma', ]
+
+length(unique(gsea_down_stroma$pathway))
+
+gsea_down_stroma <- filter(gsea_down_stroma, NES <= -2)
+
+############################################################################
+###########################################################################
+# clustering based on jaccard idx - nr of common elements in a set / union of sets
+
+paths_genes_list <- lapply(gsea_down_stroma$leadingEdge, function(x){
+  genelist <- unlist(strsplit(x, split='|', fixed=T))
+})
+
+names(paths_genes_list) <- gsea_down_stroma$pathway
+
+
+path_jaccard <- lapply(paths_genes_list, function(x){
+  p1 <- lapply(paths_genes_list, function(y){
+    jacc_idx <- as.numeric(round(length(intersect(x, y)) / length(union(x,y)), digits = 4))
+  })
+  return(unlist(p1))
+})
+
+path_jaccard_mtx <- do.call('cbind', path_jaccard)
+
+
+hist(path_jaccard_mtx, xlim = c(0, 0.5), breaks = 100)
+png('~/hmap_paths_cluster.png', width = 3500, height = 3500, pointsize = 8)
+heatmap(path_jaccard_mtx)
+dev.off()
+
+
+# clustering with dbscan
+# dbscan::kNNdistplot(path_jaccard_mtx, k =  5)
+# 
+# dbscan_res <- dbscan::dbscan(path_jaccard_mtx, eps = 1.5)
+# 
+# path_clust <- dbscan_res$cluster
+# names(path_clust) <- colnames(path_jaccard_mtx)
+# 
+# cl0 <- names(path_clust[path_clust == 0])
+# cl1 <- names(path_clust[path_clust == 1])
+# cl2 <- names(path_clust[path_clust == 2])
+
+# clustering with hclust
+path_hclust <- hclust(dist(path_jaccard_mtx), method = "average")
+plot(path_hclust, hang = -1, cex = 0.2)
+
+path_hclust_cut_05 <- cutree(path_hclust, h = 0.5)
+
+length(unique(unname(path_hclust_cut_05)))
+sort(table(path_hclust_cut_05), decreasing = T)
+
+path_hclust_cut_05[path_hclust_cut_05 == '70']
+
+#############
+
+path_hclust_cut_1 <- cutree(path_hclust, h = 1)
+
+length(unique(unname(path_hclust_cut_1)))
+sort(table(path_hclust_cut_1), decreasing = T)
+
+path_hclust_cut_1[path_hclust_cut_1 == '153']
