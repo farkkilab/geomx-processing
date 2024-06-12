e3ef8a66f662885801154d3d864fdeabd7582782
Niemiec Iga <iganiemi@lx2-304-27010.ad.helsinki.fi>
2024-06-12 14:15:19 +0300

    paired dotplots between bp and sd + cycif counts

--- src/geomx_deconvolution_comparison.R
+++ src/geomx_deconvolution_comparison.R
@@ -39,7 +39,7 @@ bp_res_files <- list.files(file.path(output_dir, 'deconvolution', 'bp'), full.na
 sd_res_files <- list.files(file.path(output_dir, 'deconvolution', 'sd'), full.names = T, pattern = 'RDS')
 
 #TODO iterate through all files
-res_path <- c(bp_res_files, sd_res_files)[8]
+res_path <- c(bp_res_files, sd_res_files)[4]
 ####################################
 # get deconvolution dirnames etc
 deconv_type <- gsub('_.*', '', basename(res_path))
@@ -230,7 +230,7 @@ sapply(c('stroma', 'tumor'), function(seg){
   ct_frac_long_seg <- ct_frac_long[ct_frac_long$Segment == seg, ]
   ct_frac_long_seg <- arrange(ct_frac_long_seg, Annotation_cell)
   
-  ggplot(data = ct_frac_long_seg, aes(x = roi_name, y = value, fill = variable)) +
+  ggplot(data = ct_frac_long_seg, aes(x = dcc_filename, y = value, fill = variable)) +
     geom_bar(position="fill", stat="identity") +
     ggtitle(seg) +
     theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1, size = 4))
@@ -240,7 +240,7 @@ sapply(c('stroma', 'tumor'), function(seg){
   
   ct_frac_long_seg_immune <- filter(ct_frac_long_seg, variable %in% cells_immune)
   
-  ggplot(data = ct_frac_long_seg_immune, aes(x = roi_name, y = value, fill = variable)) +
+  ggplot(data = ct_frac_long_seg_immune, aes(x = dcc_filename, y = value, fill = variable)) +
     geom_bar(position="fill", stat="identity") +
     ggtitle(seg) +
     theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1, size = 4))
@@ -249,12 +249,161 @@ sapply(c('stroma', 'tumor'), function(seg){
          width = 1500, height = 1000, unit = 'px')
 })
 
-###########################################################################3
+##########
+# paired dotplot compare 2 deconvolution types
+bp_path <- '/media/iganiemi/T7-iga/st/geomx-processing/results/nact2/deconvolution/bp/bp_res_mid_lvl_ct_45.RDS'
+sd_path <- '/media/iganiemi/T7-iga/st/geomx-processing/results/nact2/deconvolution/sd/sd_res_mid_lvl_ct_45_filt_scrna_geomx.RDS'
+
+macro_ct <- 'Macrophages'
+cd8_ct <- 'Tcells'
+
+bp_mid_lvl_ct <- get.fraction (bp= readRDS(bp_path),
+                               which.theta="final",
+                               state.or.type="type")
+
+sd_mid_lvl_ct <- pData(readRDS(sd_path))[, 'prop_of_all']
+
+identical(rownames(bp_mid_lvl_ct), rownames(sd_mid_lvl_ct))
+
+#bp_mid_lvl_ct <- bp_mid_lvl_ct[, c(macro_ct, cd8_ct, 'tumor')]
+colnames(bp_mid_lvl_ct) <- paste0('bp_', colnames(bp_mid_lvl_ct))
+bp_mid_lvl_ct <- rownames_to_column(as.data.frame(bp_mid_lvl_ct), 'dcc_filename')
+
+#sd_mid_lvl_ct <- sd_mid_lvl_ct[, c(macro_ct, cd8_ct, 'tumor')]
+colnames(sd_mid_lvl_ct) <- paste0('sd_', colnames(sd_mid_lvl_ct))
+sd_mid_lvl_ct <- rownames_to_column(as.data.frame(sd_mid_lvl_ct), 'dcc_filename')
+
+deconv_both <- left_join(bp_mid_lvl_ct, sd_mid_lvl_ct)
+
+deconv_long <- melt(deconv_both, id.vars = c('dcc_filename'))
+deconv_long$cell_type <- gsub('.*_', '', deconv_long$variable)
+deconv_long$deconv_type <- gsub('_.*', '', deconv_long$variable)
+
+# make a paired plot
+ggplot(deconv_long, aes(x = deconv_type, y = value)) + 
+  geom_boxplot(aes(fill = deconv_type), alpha = .2) +
+  geom_line(aes(group = dcc_filename)) + 
+  geom_point(size = 2) + 
+  facet_wrap(~ cell_type)
+
+ggsave(file.path(output_dir, 'deconvolution', paste0('bp_sd_mid_lvl_ct_deconv_comparison_all.png')),,
+       width = 1500, height = 1000, unit = 'px')
+
+# separately tumor-stroma
+geomx_meta <- sData(readRDS(input_rds_path))[, c('dcc_filename', 'Segment')]
+deconv_long <- left_join(deconv_long, geomx_meta)
+
+sapply(c('tumor', 'stroma'), function(seg){
+  ggplot(deconv_long[deconv_long$Segment == seg, ], aes(x = deconv_type, y = value)) +
+    geom_boxplot(aes(fill = deconv_type), alpha = .2) +
+    geom_line(aes(group = dcc_filename)) +
+    geom_point(size = 2) +
+    facet_wrap(~ cell_type) +
+    ggtitle(seg)
+
+  ggsave(file.path(output_dir, 'deconvolution', paste0('bp_sd_mid_lvl_ct_deconv_comparison_all_', seg, '.png')),,
+         width = 1500, height = 1000, unit = 'px')
+})
+
+
+###########################################################################
 ###########################################################################
 #############################################################################
 # adjust format from adjacent cycif cell counts
+#TODO make paired dotplot comparing bp and sd
 
+cycif_counts_path <- '/home/ad/P-drive/h345/afarkkilab/Data/9-EyeMT/Data_integration/Quantified_cells/cell_count_per_AOI.csv'
 
+cycif_counts <- fread(cycif_counts_path)
+
+cycif_counts <- cycif_counts %>%
+  mutate(all_intumor_nr = rowSums(dplyr::select(., starts_with("Intumor")))) %>%
+  mutate(all_instroma_nr = rowSums(dplyr::select(., starts_with("Instroma")))) %>%
+  filter(Note != 'ROTATE')
+
+roi_meta <- sData(readRDS(input_rds_path))[, c('dcc_filename', 'Sample', 'Segment', 'Roi')] %>%
+  mutate(Roi = as.numeric(gsub('^0*', '', Roi)))
+
+cycif_counts <- left_join(cycif_counts, roi_meta, by = c('Sample', 'Roi')) %>%
+  mutate(cells_nr_aoi = ifelse(Segment == 'tumor', rowSums(dplyr::select(., starts_with("Intumor"))),
+         ifelse(Segment == 'stroma', rowSums(dplyr::select(., starts_with("Instroma"))), NA))) %>%
+  mutate(frac_aoi_tumor = ifelse(Segment == 'tumor', Intumor_Tumor/cells_nr_aoi, ifelse(Segment == 'stroma',
+                                                                         Instroma_Tumor/cells_nr_aoi, NA))) %>%
+  mutate(frac_aoi_cd8 = ifelse(Segment == 'tumor', Intumor_CD8Tcells/cells_nr_aoi, ifelse(Segment == 'stroma',
+                                                                                          Instroma_CD8Tcells/cells_nr_aoi, NA))) %>%
+  mutate(frac_aoi_macro = ifelse(Segment == 'tumor', Intumor_Macrophages/cells_nr_aoi, ifelse(Segment == 'stroma',
+                                                                                              Instroma_Macrophages/cells_nr_aoi, NA)))
+
+# add counts from deconvolution
+bp_mid_lvl_ct <- get.fraction (bp= readRDS('/media/iganiemi/T7-iga/st/geomx-processing/results/nact2/deconvolution/bp/bp_res_mid_lvl_ct_45.RDS'),
+                               which.theta="final",
+                               state.or.type="type")
+
+#colnames(bp_mid_lvl_ct) <- paste0('deconv_', colnames(bp_mid_lvl_ct))
+bp_mid_lvl_ct <- rownames_to_column(as.data.frame(bp_mid_lvl_ct), 'dcc_filename')
+bp_mid_lvl_ct <- bp_mid_lvl_ct[, c('dcc_filename', 'tumor', 'Tcells', 'Macrophages')]
+colnames(bp_mid_lvl_ct) <- c('dcc_filename', 'deconv_tumor', 'deconv_cd8', 'deconv_macro')
+# bp_cell_type <- get.fraction (bp= readRDS('/media/iganiemi/T7-iga/st/geomx-processing/results/nact2/deconvolution/bp/bp_res_cell_type_45.RDS'),
+#                                which.theta="final",
+#                                state.or.type="type")
+
+cycif_counts <- left_join(cycif_counts, bp_mid_lvl_ct)
+
+# make it long
+cycif_counts_long <- cycif_counts[, c('dcc_filename', 'Sample', 'Roi', 'Segment', 'cells_nr_aoi',
+                                      "frac_aoi_tumor", "frac_aoi_cd8", "frac_aoi_macro",
+                                      "deconv_tumor", "deconv_cd8", "deconv_macro")]
+cycif_counts_long <- melt(cycif_counts_long, id.vars = c('dcc_filename', 'Sample', 'Roi', 'Segment', 'cells_nr_aoi'))
+cycif_counts_long$cell_type <- gsub('.*_', '', cycif_counts_long$variable)
+cycif_counts_long$count_type <- gsub('_.*', '', cycif_counts_long$variable)
+
+# make a paired plot
+ggplot(cycif_counts_long, aes(x = count_type, y = value)) + 
+  geom_boxplot(aes(fill = count_type), alpha = .2) +
+  geom_line(aes(group = dcc_filename)) + 
+  geom_point(size = 2) + 
+  facet_wrap(~ cell_type)
+
+ggsave(file.path(output_dir, 'deconvolution', paste0('cycif_count_deconv_comparison_all.png')),,
+       width = 1500, height = 1000, unit = 'px')
+
+# only S084_post where are cells in both
+ggplot(cycif_counts_long[cycif_counts_long$Sample == 'S084_post', ], aes(x = count_type, y = value)) + 
+  geom_boxplot(aes(fill = count_type), alpha = .2) +
+  geom_line(aes(group = dcc_filename)) + 
+  geom_point(size = 2) + 
+  facet_wrap(~ cell_type)+
+  ggtitle('S084_post')
+
+ggsave(file.path(output_dir, 'deconvolution', paste0('cycif_count_deconv_comparison_S084_post.png')),,
+       width = 1500, height = 1000, unit = 'px')
+
+#############################################
+#############################################
+# tumor/stroma separately
+sapply(c('tumor', 'stroma'), function(seg){
+  ggplot(cycif_counts_long[cycif_counts_long$Segment == seg, ], aes(x = count_type, y = value)) + 
+    geom_boxplot(aes(fill = count_type), alpha = .2) +
+    geom_line(aes(group = dcc_filename)) + 
+    geom_point(size = 2) + 
+    facet_wrap(~ cell_type) +
+    ggtitle(seg)
+  
+  ggsave(file.path(output_dir, 'deconvolution', paste0('cycif_count_deconv_comparison_all_', seg, '.png')),,
+         width = 1500, height = 1000, unit = 'px')
+  
+  # only S084_post where are cells in both
+  ggplot(cycif_counts_long[(cycif_counts_long$Sample == 'S084_post' & cycif_counts_long$Segment == seg), ], 
+         aes(x = count_type, y = value)) + 
+    geom_boxplot(aes(fill = count_type), alpha = .2) +
+    geom_line(aes(group = dcc_filename)) + 
+    geom_point(size = 2) + 
+    facet_wrap(~ cell_type)+
+    ggtitle(paste0(seg, 'S084_post'))
+  
+  ggsave(file.path(output_dir, 'deconvolution', paste0('cycif_count_deconv_comparison_S084_post_', seg, '.png')),,
+         width = 1500, height = 1000, unit = 'px')
+})
 
 #############################################
 #############################################
