6d890c6554797276ad533c466572db121853130b
Niemiec Iga <iganiemi@lx2-304-27010.ad.helsinki.fi>
2024-07-04 13:35:56 +0300

    relabel based on sd

--- src/geomx_relabel_roi.R
+++ src/geomx_relabel_roi.R
@@ -22,8 +22,10 @@ output_dir <- '/media/iganiemi/T7-iga/st/geomx-processing/results/nact2'
 
 input_rds_path <- file.path(output_dir, 'geomx_qc_norm.RDS')
 
-deconv_path_mid <- file.path(output_dir, 'deconvolution', 'bp/bp_res_mid_lvl_ct_45.RDS')
-deconv_path_ct <- file.path(output_dir, 'deconvolution', 'bp/bp_res_cell_type_45.RDS')
+deconv_path_mid_bp <- file.path(output_dir, 'deconvolution', 'bp/bp_res_mid_lvl_ct_45.RDS')
+#deconv_path_ct_bp <- file.path(output_dir, 'deconvolution', 'bp/bp_res_cell_type_45.RDS')
+
+deconv_path_mid_sd <- file.path(output_dir, 'deconvolution', 'sd/sd_res_mid_lvl_ct_nofilt.rds')
 
 # make dirs and source functions ------------------------------------------
 
@@ -38,28 +40,34 @@ meta_names <- c('dcc_filename', 'Patient', 'Segment', 'Sample', 'NACT status', '
 cd8_ct <- 'Tcells'
 macro_ct <- 'Macrophages'
 
-deconv_res_mid <- readRDS(deconv_path_mid)
-deconv_res_ct <- readRDS(deconv_path_ct)
+deconv_res_mid_bp <- readRDS(deconv_path_mid_bp)
+# deconv_res_ct_bp <- readRDS(deconv_path_ct_bp)
 
-
-frac_mid <- get.fraction (bp= deconv_res_mid,
+frac_mid_bp <- get.fraction (bp= deconv_res_mid_bp,
                                which.theta="final",
                                state.or.type="type")
 
-frac_mid <- rownames_to_column(as.data.frame(frac_mid), 'dcc_filename')
-frac_mid <- left_join(frac_mid, sData(geomx_obj)[, c(meta_names)])
+frac_mid_bp <- rownames_to_column(as.data.frame(frac_mid_bp), 'dcc_filename')
+frac_mid_bp <- left_join(frac_mid_bp, sData(geomx_obj)[, c(meta_names)])
 
+########################
+sd_deconv <- readRDS(deconv_path_mid_sd)
+sd_deconv <- data.frame(pData(sd_deconv)[, 'prop_of_all'])
+sd_deconv <- tibble::rownames_to_column(sd_deconv, 'dcc_filename')
+sd_deconv <- left_join(sd_deconv, sData(geomx_obj)[, c(meta_names)])
 #TODO doubleneg only if doubleneg not lost in QC etc 
 #TODO calculate how many relabeled per sample
+
+frac_mid <- sd_deconv
 
 # relabel rois per sample -------------------------------------------------
 
 frac_sample_all <- lapply(unique(frac_mid$Sample), function(sample_name){
-  frac_sample <- frac_mid[frac_mid$Sample == sample_name,]
+  frac_sample <- frac_mid_bp[frac_mid$Sample == sample_name,]
   
   frac_sample_roi <- group_by(frac_sample, Roi) %>%
-    summarise(macro_sum = sum(Macrophages)/2, cd8_sum = sum(Tcells)/2, nseg = n()) %>%
-    filter(nseg == 2) # remove rois where one segment was removed due to qc 
+    dplyr::summarise(macro_sum = sum(Macrophages)/2, cd8_sum = sum(Tcells)/2, nseg = n()) %>%
+    dplyr::filter(nseg == 2) # remove rois where one segment was removed due to qc 
   
   min_macro <- frac_sample_roi$Roi[which.min(frac_sample_roi$macro_sum)]
   min_tcell <- frac_sample_roi$Roi[which.min(frac_sample_roi$cd8_sum)]
@@ -87,4 +95,4 @@ relabeled_rois <- frac_sample_all[frac_sample_all$Annotation_cell != frac_sample
 
 # save relabeled df
 
-fwrite(frac_sample_all, file.path(output_dir, 'deconvolution', 'bp_mid_lvl_ct_relabeled_roi.csv'))
+fwrite(frac_sample_all, file.path(output_dir, 'deconvolution', 'sd_mid_lvl_ct_relabeled_roi.csv'))
