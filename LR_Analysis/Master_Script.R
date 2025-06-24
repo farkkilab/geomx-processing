ad97d176e6d72e1cfc9153c1d12f0a3d2c7f1a24
iamgayani <ananda.gaya.ac@gmail.com>
2025-06-24 22:42:50 +0300

    fixed the script to run for bulksignalr combined data

--- LR_Analysis/Master_Script.R
+++ LR_Analysis/Master_Script.R
@@ -50,22 +50,20 @@ cell_fractions_df = read.csv(paste0(data_dir,"/bp_res_mid_lvl_ct_ct_fraction.csv
 # set up metadata variables names -----------------------------------------
 aoi_id <- 'dcc_filename'
 sample_name <- 'Sample'
-aoi_segment_var <<- "Segment"
-main_experimental_condition <<- 'NACT_status' # eg 'NACT_status', but NULL for demo data
+aoi_segment_var <- "Segment"
+main_experimental_condition <- 'NACT_status' # eg 'NACT_status', but NULL for demo data
 grouping_var_col_ids <- c("Segment") # define the meta data column names of the groups that needed to be compared eg: c("NACT_status","Segment")
+
 
 # params  -----------------------------------------
 
-# parameters for BulkSignalR
-
 comparison <- c("stroma","tumor") # order of the group should matches with the order of the column names eg: c("pre-stroma","pre-tumor")
 
-# TODO Run for combined data 
-combined_Data = FALSE # Run for combined data
+# parameters for BulkSignalR
+combined_Data = TRUE # To run for combined data as well
 
 
 # parameters for CellChat
-
 cell_types = c("Tcells","Macrophages") # set to NULL to get all the cell types : ct_of_interest
 
 
@@ -84,18 +82,7 @@ dir.create(output_dir, recursive = T, showWarnings = F)
 # define intermediate output paths ----------------------------------------
 
 
-
-# if (combined_Data == TRUE) {
-#   
-#   geomx_BulkSignalR_path <<- file.path(output_dir,'BulkSignalR_objects' ,'BulkSignalR_combined_output.RDS')
-#   
-# } else {
-#   
-#   file_name = paste(parts_non_NA, collapse = "_")
-#   geomx_BulkSignalR_path <<- file.path(output_dir, 'BulkSignalR_objects',paste0('BulkSignalR_',file_name,'_output.RDS'))
-#   
-# }
-
+geomx_BulkSignalR_path <<- file.path(output_dir,'BulkSignalR_objects' ,'BulkSignalR_combined_output.RDS')
 geomx_CellChat_path <<- file.path(output_dir, 'CellChat_objects','CellChat_output.RDS')
 geomx_MultiNicheNet_path <<- file.path(output_dir, 'MultiNicheNet_objects','multinichenet_output.rds')
 
