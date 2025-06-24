f77819bf0987a8418aa3a5d17d0000cbc57a94d6
iamgayani <ananda.gaya.ac@gmail.com>
2025-06-24 17:02:10 +0300

    soft coded the grouping variables in cellchat

--- LR_Analysis/Master_Script.R
+++ LR_Analysis/Master_Script.R
@@ -5,11 +5,11 @@
 library(BulkSignalR, quietly =T)
 library(igraph, quietly =T)
 library(dplyr, quietly =T)
-# library(Seurat, quietly =T)
-# library(SeuratObject, quietly =T)
 library(DESeq2, quietly =T)
 library(CellChat, quietly =T)
 library(patchwork, quietly =T)
+library(rlang)
+library(purrr)
 
 
 # TODO check the libraries needed
@@ -23,7 +23,10 @@ library(GeomxTools, quietly =T)
 library(ggplot2, quietly =T)
 
 
-
+# For MultiNicheNetr
+library(SingleCellExperiment, quietly =T)
+library(nichenetr, quietly =T)
+library(multinichenetr, quietly =T)
 
 
 
@@ -36,14 +39,26 @@ data_dir <- file.path(proj_dir, 'Batch01_Data')
 output_dir <<- file.path(proj_dir, 'results', 'Batch01') 
 
 
+# load data ----------------------------------------------
+
+
+geomx_obj = readRDS(paste0(data_dir,"/geomx_qc_norm_batch_eff_rm.RDS"))
+bprism_res = readRDS(paste0(data_dir,"/bp_res_mid_lvl_ct.RDS"))
+cell_fractions_df = read.csv(paste0(data_dir,"/bp_res_mid_lvl_ct_ct_fraction.csv"))
+
+
+# set up metadata variables names -----------------------------------------
+aoi_id <- 'dcc_filename'
+sample_name <- 'Sample'
+aoi_segment_var <<- "Segment"
+main_experimental_condition <<- 'NACT_status' # eg 'NACT_status', but NULL for demo data
+grouping_var_col_ids <- c("Segment") # define the meta data column names of the groups that needed to be compared eg: c("NACT_status","Segment")
+
 # params  -----------------------------------------
 
 # parameters for BulkSignalR
-# keep all as NA to get the LR prediction for the whole dataset
 
-nact_status = NA # define as C("pre","post")
-segment = c("stroma")
-annotation = NA
+comparison <- c("stroma","tumor") # order of the group should matches with the order of the column names eg: c("pre-stroma","pre-tumor")
 
 # TODO Run for combined data 
 combined_Data = FALSE # Run for combined data
@@ -51,7 +66,7 @@ combined_Data = FALSE # Run for combined data
 
 # parameters for CellChat
 
-cell_types = c("Tcells","Macrophages") # set to NULL to get all the cell types 
+cell_types = c("Tcells","Macrophages") # set to NULL to get all the cell types : ct_of_interest
 
 
 # parameters for MultiNicheNet
@@ -61,36 +76,33 @@ cell_types = c("Tcells","Macrophages") # set to NULL to get all the cell types
 
 # load util functions and create dirs -------------------------------------
 
-source(file.path(proj_dir, 'src', 'geomx_LR_utils.R'))
-source(file.path(proj_dir, 'src', 'cellChat_util.R'))
+source(file.path(proj_dir, 'LR_Analysis', 'BulkSignalR_LR_utils.R'))
+source(file.path(proj_dir, 'LR_Analysis', 'cellChat_util.R'))
 
 dir.create(output_dir, recursive = T, showWarnings = F)
 
 # define intermediate output paths ----------------------------------------
 
-parts <- c(nact_status, segment, annotation)
-parts_non_NA <- parts[!sapply(parts, is.na)]
-
-if (length(parts_non_NA) == 0) {
-  
-  geomx_BulkSignalR_path <<- file.path(output_dir,'BulkSignalR_objects' ,'BulkSignalR_combined_output.RDS')
-  
-} else {
-  
-  file_name = paste(parts_non_NA, collapse = "_")
-  geomx_BulkSignalR_path <<- file.path(output_dir, 'BulkSignalR_objects',paste0('BulkSignalR_',file_name,'_output.RDS'))
-  geomx_CellChat_path <<- file.path(output_dir, 'CellChat_objects',paste0('CellChat_',file_name,'_output.RDS'))
-}
 
 
+# if (combined_Data == TRUE) {
+#   
+#   geomx_BulkSignalR_path <<- file.path(output_dir,'BulkSignalR_objects' ,'BulkSignalR_combined_output.RDS')
+#   
+# } else {
+#   
+#   file_name = paste(parts_non_NA, collapse = "_")
+#   geomx_BulkSignalR_path <<- file.path(output_dir, 'BulkSignalR_objects',paste0('BulkSignalR_',file_name,'_output.RDS'))
+#   
+# }
 
-
-
+geomx_CellChat_path <<- file.path(output_dir, 'CellChat_objects','CellChat_output.RDS')
+geomx_MultiNicheNet_path <<- file.path(output_dir, 'MultiNicheNet_objects','multinichenet_output.rds')
 
 # conditionally run BulkSignaR Analysis  -----------------------------------------
 
-run_unless_exists('BulkSignaR LR Analysis', geomx_BulkSignalR_path, 
-                  file.path(proj_dir, 'src', 'BulkSignaR_LR_Analysis.R'))
+run_unless_exists('BulkSignaR LR Analysis', geomx_BulkSignalR_path,
+                  file.path(proj_dir, 'LR_Analysis', 'BulkSignaR_LR_Analysis.R'))
 
 
 # BulkSignalR Visualization -----------------------------------------
@@ -100,7 +112,7 @@ run_unless_exists('BulkSignaR LR Analysis', geomx_BulkSignalR_path,
 # conditionally run CellChat LR Analysis -----------------------------------------
 
 run_unless_exists('CellChat LR Analysis', geomx_CellChat_path, 
-                  file.path(proj_dir, 'src', 'CellChat_LR_Analysis.R'))
+                  file.path(proj_dir, 'LR_Analysis', 'CellChat_LR_Analysis.R'))
 
 
 # CellChat Visualization -----------------------------------------
@@ -110,7 +122,8 @@ run_unless_exists('CellChat LR Analysis', geomx_CellChat_path,
 
 # conditionally run MultiNicheNet LR Analysis -----------------------------------------
 
-
+run_unless_exists('MultiNicheNet LR Analysis', geomx_MultiNicheNet_path,
+                  file.path(proj_dir, 'LR_Analysis', 'MultiNicheNet_LR_Analysis.R'))
 
 
 
