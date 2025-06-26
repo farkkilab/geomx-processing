78320cce706360f1c5ecde90fbd939cb231e65e6
iamgayani <ananda.gaya.ac@gmail.com>
2025-06-26 15:14:26 +0300

    added bubble plot

--- LR_Analysis/BulkSignaR_LR_Visualization.R
+++ LR_Analysis/BulkSignaR_LR_Visualization.R
@@ -5,7 +5,8 @@
 
 qval_threshold = 0.001 # filter significant LR pairs
 n = 50 # number of top LR pairs needed to visualize in the signature scoes heatmap
-
+heatmap_col_ann = "segment" # based on what you want to annotate the heatmap
+LR_corr_threshold = 0.4 
 
 # for plots
 
@@ -28,7 +29,7 @@ pathway_names = pathway$Pathway.names
 
 
 # Generate a heatmap
-plot_heatmap = plot_heatmap(bsrinf.redBP, bsrdm, meta_data, pathway_names, qval_threshold, n, grouping_var_col_ids)
+plot_heatmap = plot_heatmap(bsrinf.redBP, bsrdm, meta_data, pathway_names, qval_threshold, n, heatmap_col_ann)
 
 
 # save in a pdf
@@ -42,7 +43,68 @@ dev.off()
 
 # Bubble plots if you are comparing between groups
 # First need to generate LR pairs separately for each group
-# Define what groups you need to compare : eg stroma vs tumor  
+# provide pathways you want to visualize as a .csv file in the master script 
+
+
+
+lr_df_list = list()
+
+for (group in comparison){
+  
+  df = BulkSignaR_Output[[group]]$LRinter_pairs_best_pws
+  df$group = group
+  lr_df_list[[group]] = df
+  
+}
+
+df_combined = do.call(rbind, lr_df_list)
+
+
+# TODO need to group based on pathway
+
+df_plot = df_combined %>% filter(qval < qval_threshold,
+                                 LR.corr > LR_corr_threshold,
+                                 pw.name %in% pathway_names)
+
+df_plot$lr_interaction = paste0(df_plot$L,"-",df_plot$R)
+df_plot$neg_log10_p_adj = -log(df_plot$qval)
+
+
+p1 =  df_plot %>%
+  ggplot(aes(group, lr_interaction, color = LR.corr, size = neg_log10_p_adj)) +
+  geom_point() +
+  facet_grid(pw.name~group, scales = "free", space = "free", switch = "y")+
+  scale_x_discrete(position = "top") +
+  theme_light() +
+  theme(
+    axis.ticks = element_blank(),
+    axis.title = element_blank(),
+    #axis.text.y = element_blank(),
+    axis.text.y = element_text(face = "bold.italic", size = 7),
+    axis.text.x = element_blank(),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    panel.spacing.x = unit(0.40, "lines"),
+    panel.spacing.y = unit(0.25, "lines"),
+    strip.text.x.top = element_text(size = 8, color = "black", face = "bold", angle = 0),
+    strip.text.y.left = element_text(size = 9, color = "black", face = "bold", angle = 0),
+    strip.background = element_rect(color="darkgrey", fill="whitesmoke", size=1.5, linetype="solid")
+  ) + labs(color = "Median difference\nin scaled L-R\npseudobulk\nexpression\nproduct", size = "-log10(pval_adj)") 
+
+max_corr = abs(df_plot$LR.corr) %>% max()
+
+custom_scale_fill = scale_color_gradientn(
+  colours = RColorBrewer::brewer.pal(n = 7, name = "PuRd"),
+  values = c(0, 0.5,0.6,0.7,0.8,0.9,1),  
+  limits = c(0,max_corr))
+
+p1 = p1+ custom_scale_fill + scale_size_binned_area(max_size = 4) 
+
+
+
+pdf(file.path(plot_dir, "bubble_plot.pdf"), width = 9, height = 7)
+print(p1)
+dev.off()
 
 
 
@@ -51,6 +113,13 @@ dev.off()
 
 
 
+
+
+    
+    
+    
+    
+    
 
 
 
