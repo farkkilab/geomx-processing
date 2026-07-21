8ad20fb01a344d9472aae814ce6ea46808146534
Niemiec Iga <iganiemi@lx2-304-27010.ad.helsinki.fi>
2026-07-21 12:45:30 +0300

    full run, fixed group comparisons, visualisation added

--- src/lr-analysis/multinichenetr_run2.R
+++ src/lr-analysis/multinichenetr_run2.R
@@ -14,26 +14,40 @@ library(data.table)
 library(dplyr)
 library(tibble)
 
-dir.create(file.path(output_dir, 'lr_interactions', 'multi_niche_netr') , recursive = T, showWarnings = F)
-geomx_MultiNicheNet_path <<- file.path(output_dir, 'lr_interactions', 'multi_niche_netr', 'multinichenet_output.rds')
+
+# from master file
+proj_dir <<- '~/Documents/phd/st'
+output_dir <<- file.path(proj_dir, 'geomx-processing', 'results', 'batch123-2808')
+geomx_norm_batch_eff_rm_path <<- file.path(output_dir, 'geomx_qc_norm_batch_eff_rm.RDS') 
+
+aoi_id <<- 'dcc_filename'
+roi_id <<- 'Roi_geomx'
+aoi_segment_var <<- "Segment"
+sample_name <- "Sample"
+main_experimental_condition <<- 'NACT_status'
+
+custom_metadt_path <<- "~/Documents/phd/st/data/geomx/metadata_full_SENSITIVE.csv"
+dge_dir <- "/home/iganiemi/Documents/phd/st/geomx-processing/results/batch123-2808/dge/dge_within_slide_roi_cluster_label_gmm_bin_TRUE_Macro_domin_Segment_NACT_status"
+
+
+plot_dir = file.path(output_dir, 'lr_interactions', 'multi_niche_netr','plots_and_csv_files')
+dir.create(plot_dir , recursive = T, showWarnings = F)
 
 # path where to download nichenet data
 nichenet_data_dir <<- file.path(proj_dir, 'geomx-processing', 'data', 'nichenet')
 dir.create(nichenet_data_dir, recursive = T, showWarnings = F)
 
-plot_dir = file.path(output_dir, 'lr_interactions', 'multi_niche_netr','plots_and_csv_files')
-dir.create(plot_dir , recursive = T, showWarnings = F)
-
-custom_metadt_path <<- "~/Documents/phd/st/data/geomx/metadata_full_SENSITIVE.csv"
-dge_dir <- "/home/iganiemi/Documents/phd/st/geomx-processing/results/batch123-2808/dge/dge_within_slide_roi_cluster_label_gmm_bin_TRUE_Macro_domin_Segment_NACT_status"
-
+source(file.path(proj_dir, 'geomx-processing', 'src', 'geomx_utils.R'))
+source(file.path(proj_dir, 'geomx-processing', 'src', 'lr-analysis', 'MultiNicheNet_LR_Visualization.R'))
 
 # define params -----------------------------------------------------------
 
 scrna_anno <<- 'mid_lvl_ct_updated' #either 'cell_type' / 'mid_lvl_ct' / 'mid_lvl_ct_updated' / 'low_lvl_ct'
 
 # names of cells to fin
-cell_types_selected <- c("Macrophages_Monocytes", "Tcells_CD8", "DCs", "Fibroblasts_Mesothelial", "Bcells")
+# c("tumor", "Macrophages_Monocytes", "Tcells_CD8", "Tcells_CD4", "DCs", "Fibroblasts_Mesothelial", "Bcells")
+
+cell_types_selected <- c("Macrophages_Monocytes", "Tcells_CD8")
 
 # for multiNicheNetR if you are providing DEGS externally follow the MultiNicheNet_LR_util.R script to prepare the DEGs dataframe. 
 # Else MUltiNicheNet will not work
@@ -78,28 +92,35 @@ bp_pseudosc_path <- file.path(output_dir, 'lr_interactions',
 
 outct <- ifelse(!is.null(cell_types_selected), paste(cell_types_selected, collapse = '_'), 'all')
 
+geomx_MultiNicheNet_path <<- file.path(output_dir, 'lr_interactions', 'multi_niche_netr', 
+                                       paste0('multinichenet_output_', outct, '.rds'))
 
 # load files --------------------------------------------------------------
 
 organism = "human"
 
-lr_network_all = 
-  readRDS(url(
-    "https://zenodo.org/record/10229222/files/lr_network_human_allInfo_30112033.rds"
-  )) %>% 
+if(!file.exists(file.path(nichenet_data_dir, "lr_network_human_allInfo_30112033.rds"))){
+  download.file('https://zenodo.org/record/10229222/files/lr_network_human_allInfo_30112033.rds', 
+                destfile = file.path(nichenet_data_dir, "lr_network_human_allInfo_30112033.rds"), method = "wget", extra = "-r -p --random-wait")
+}
+
+if(!file.exists(file.path(nichenet_data_dir, "ligand_target_matrix_nsga2r_final.rds"))){
+  download.file('https://zenodo.org/record/7074291/files/ligand_target_matrix_nsga2r_final.rds', 
+                destfile = file.path(nichenet_data_dir, "ligand_target_matrix_nsga2r_final.rds"), method = "wget", extra = "-r -p --random-wait")
+  
+}
+
+lr_network_all <- readRDS(file.path(nichenet_data_dir, "lr_network_human_allInfo_30112033.rds"))
+ligand_target_matrix <- readRDS(file.path(nichenet_data_dir, "ligand_target_matrix_nsga2r_final.rds"))
+
+lr_network_all = lr_network_all %>%
   mutate(
     ligand = convert_alias_to_symbols(ligand, organism = organism), 
-    receptor = convert_alias_to_symbols(receptor, organism = organism))
-
-lr_network_all = lr_network_all  %>% 
+    receptor = convert_alias_to_symbols(receptor, organism = organism)) %>%
   mutate(ligand = make.names(ligand), receptor = make.names(receptor)) 
 
 lr_network = lr_network_all %>% 
   distinct(ligand, receptor)
-
-ligand_target_matrix = readRDS(url(
-  "https://zenodo.org/record/7074291/files/ligand_target_matrix_nsga2r_final.rds"
-))
 
 colnames(ligand_target_matrix) = colnames(ligand_target_matrix) %>% 
   convert_alias_to_symbols(organism = organism) %>% make.names()
@@ -122,7 +143,7 @@ meta_data_all$roi_cluster_label_gmm <- ifelse(meta_data_all$roi_cluster_label_gm
 
 # TODO code repetition from BSR Analysis
 # make variable with all categories from grouping_var_col_ids
-meta_data_all$groupes_to_compare <- apply(meta_data_all, 1, function(row){
+meta_data_all$groups_to_compare <- apply(meta_data_all, 1, function(row){
   group <- sapply(grouping_var_col_ids, function(var){
     paste(row[var])
   })
@@ -137,7 +158,7 @@ bprism_res_raw <- fread(bp_pseudosc_path) %>%
   column_to_rownames('V1')
 
 # make metadata -----------------------------------------------------------
-group_id <- 'groupes_to_compare'
+group_id <- 'groups_to_compare'
 celltype_id <- 'ct_label'
 
 dcc_ct <- data.frame('dcc_filename' = gsub('_.*', '', colnames(bprism_res_raw)), 
@@ -170,7 +191,9 @@ dge_all <- do.call(rbind, dge_all)
 dge_all$cont1 <- paste0(dge_all$data_group, '_', gsub(' -.*', '', dge_all$Contrast))
 dge_all$cont2 <- paste0(dge_all$data_group, '_', gsub('.*- ', '', dge_all$Contrast))
 
-groups_to_compare <- unique(c(unique(dge_all$cont1), unique(dge_all$cont2)))
+if(is.null(groups_to_compare)){
+  groups_to_compare <- unique(c(unique(dge_all$cont1), unique(dge_all$cont2)))
+}
 
 # in original nn theres tum-str and str-tum contrast with negatives of each value
 celltype_de_pos <- data.frame(gene = dge_all$Gene, cluster_id = dge_all$cluster_id, logFC = dge_all$Estimate,
@@ -182,6 +205,13 @@ celltype_de_neg <- data.frame(gene = dge_all$Gene, cluster_id = dge_all$cluster_
 
 celltype_de <- rbind(celltype_de_neg, celltype_de_pos)
 
+# filter to groups to compare
+celltype_de <- celltype_de[grepl(paste0(groups_to_compare, collapse = '|'), celltype_de$contrast), ]
+
+
+rm(celltype_de_neg)
+rm(celltype_de_pos)
+rm(dge_all)
 
 # make contrasts ----------------------------------------------------------
 
@@ -217,16 +247,14 @@ contrasts_oi <- paste(contrasts_oi_list, collapse = ',')
 contrast_tbl <- tibble(contrast = contrasts_oi_list, 
                        group = unique(groups_to_compare))
 
-# contrasts_oi = c("'M-(S+A)/2','S-(M+A)/2','A-(S+M)/2'")
-# contrast_tbl = tibble(contrast = 
-#                         c("M-(S+A)/2","S-(M+A)/2", "A-(S+M)/2"), 
-#                       group = c("M","S","A"))
+# fix contrast table
+contrast_tbl$contrast <- gsub("'", "", contrast_tbl$contrast) # in NichenetR 2.1 it goes wo quites
 
 # filter metadata and expression to group, cells and samples ------------
 
 # keep only cell types and conditions of interest
 meta_data_ct <- meta_data_ct[meta_data_ct$ct_label %in% cell_types_selected & 
-                               meta_data_ct$groupes_to_compare %in% groups_to_compare, ]
+                               meta_data_ct$groups_to_compare %in% groups_to_compare, ]
 
 # # keep only AOIs where all cells are present
 # samples_w_all_cells <- meta_data_ct %>%
@@ -256,14 +284,14 @@ SummarizedExperiment::colData(sce)[[group_id]] = SummarizedExperiment::colData(s
 #SummarizedExperiment::colData(sce)[[batches]] = SummarizedExperiment::colData(sce)[[batches]] %>% make.names()
 #SummarizedExperiment::colData(sce)[[covariates]] = SummarizedExperiment::colData(sce)[[covariates]] %>% make.names()
 
-rm(bprism_res_raw)
-gc()
-
 senders_oi = SummarizedExperiment::colData(sce)[,celltype_id] %>% unique()
 receivers_oi = SummarizedExperiment::colData(sce)[,celltype_id] %>% unique()
 sce = sce[, SummarizedExperiment::colData(sce)[,celltype_id] %in% 
             c(senders_oi, receivers_oi)
 ]
+
+rm(bprism_res_raw)
+gc()
 
 # calculate abundance info ------------------------------------------------
 
@@ -422,9 +450,9 @@ ligand_activities_targets_DEgenes = suppressMessages(suppressWarnings(
 ))
 
 
-# rank cell-cell communication patterns through multi-criteria pri --------
+# prioritise cell-cell communication patterns through multi-criteria --------
 
-ligand_activity_down = FALSE
+ligand_activity_down = TRUE
 
 sender_receiver_tbl = sender_receiver_de %>% distinct(sender, receiver)
 metadata_combined = SummarizedExperiment::colData(sce) %>% tibble::as_tibble()
@@ -433,7 +461,10 @@ grouping_tbl = metadata_combined[,c(sample_id, group_id)] %>%
   tibble::as_tibble() %>% distinct()
 colnames(grouping_tbl) = c("sample","group")
 
-prioritization_tables = suppressMessages(generate_prioritization_tables(
+# These should have perfect matches (same strings, same order)
+all(contrast_tbl$contrast %in% unique(sender_receiver_de$contrast))  # Should return TRUE
+
+prioritization_tables = generate_prioritization_tables(
   sender_receiver_info = abundance_expression_info$sender_receiver_info,
   sender_receiver_de = sender_receiver_de,
   ligand_activities_targets_DEgenes = ligand_activities_targets_DEgenes,
@@ -441,12 +472,11 @@ prioritization_tables = suppressMessages(generate_prioritization_tables(
   sender_receiver_tbl = sender_receiver_tbl,
   grouping_tbl = grouping_tbl,
   scenario = "regular", # all prioritization criteria will be weighted equally
-  fraction_cutoff = fraction_cutoff, 
+  fraction_cutoff = 0.05, 
   abundance_data_receiver = abundance_expression_info$abundance_data_receiver,
   abundance_data_sender = abundance_expression_info$abundance_data_sender,
   ligand_activity_down = ligand_activity_down
-))
-
+)
 
 # across-samples expression correlation between LR and target genes --------
 
@@ -461,7 +491,6 @@ lr_target_prior_cor = lr_target_prior_cor_inference(
   p_val_threshold = p_val_threshold, 
   p_val_adj = p_val_adj
 )
-
 
 # save all the output -----------------------------------------------------
 
@@ -480,3 +509,170 @@ multinichenet_output = make_lite_output(multinichenet_output)
 
 saveRDS(multinichenet_output, geomx_MultiNicheNet_path)
 
+
+# get top LR pairs for each communication ---------------------------------
+# get top n LR pairs from each cell type communication combinations and save it as multiple csv files
+# theses df can be directly used for plotting or can do manually filtering and provide seperetly
+
+top_n_LR_pairs = list()
+top_n <- 100
+
+
+for (group in groups_to_compare) {
+  for (receiver in cell_types_selected) {
+    for (sender in cell_types_selected) {
+      
+      print(paste0(group,"-",receiver,"-",sender))
+      prioritized_tbl_oi = get_top_n_lr_pairs(
+        prioritization_tables, 
+        top_n, 
+        groups_oi = group, 
+        receivers_oi = receiver,
+        senders_oi = sender)
+      
+      
+      # ligand-receptor pseudobulk product expression panel
+      sample_data = prioritization_tables$sample_prioritization_tbl %>% 
+        dplyr::filter(id %in% prioritized_tbl_oi$id) %>% 
+        dplyr::mutate(
+          sender_receiver = paste(sender, receiver, sep = " --> "), 
+          lr_interaction = paste(ligand, receptor, sep = " - ")) %>%
+        dplyr::arrange(receiver) %>% 
+        dplyr::group_by(receiver) %>%  
+        dplyr::arrange(sender, .by_group = TRUE)
+      
+      sample_data = sample_data %>% 
+        dplyr::mutate(sender_receiver = factor(
+          sender_receiver, 
+          levels = sample_data$sender_receiver %>% unique()
+        ))
+      
+      ################################################################
+      
+      keep_sender_receiver_values = c(0.25, 0.9, 1.75, 4) # TODO check
+      names(keep_sender_receiver_values) = levels(sample_data$keep_sender_receiver)
+      
+      ######## calculate the median bulk expression for each group
+      
+      # calculate the median
+      
+      group_medians <- sample_data %>%
+        group_by(group,lr_interaction) %>%
+        summarize(median_scaled_LR = median(scaled_LR_pb_prod, na.rm = TRUE), .groups = "drop") %>%
+        pivot_wider(names_from = group, values_from = median_scaled_LR)
+      
+      
+      # Compute median difference (stroma - tumor)
+      group_medians <- group_medians %>%
+        mutate(
+          diff_median = .[[comparison[1]]] - .[[comparison[2]]]
+        )
+      
+      # Wilcoxon test per interaction
+      wilcox_results <- sample_data %>%
+        group_by(lr_interaction) %>%
+        filter(group %in% comparison) %>%
+        summarize(
+          test = list(wilcox.test(scaled_LR_pb_prod ~ group)),
+          .groups = "drop"
+        ) %>%
+        mutate(
+          p_value = map_dbl(test, "p.value"),
+          neg_log10_p = -log10(p_value)
+        ) %>%
+        select(lr_interaction, p_value, neg_log10_p)
+      
+      
+      adj_pvals <- p.adjust(wilcox_results$p_value, method = "BH")
+      
+      # Merge with fold change data
+      final_data <- group_medians %>%
+        left_join(wilcox_results, by = "lr_interaction")
+      
+      
+      final_data$adj_p_value = adj_pvals
+      final_data$neg_log10_p_adj = -log10(adj_pvals)
+      
+      
+      sender_receiver <- paste(sender, receiver, sep = " --> ")
+      final_data$sender_receiver <- rep(sender_receiver, nrow(final_data))
+      final_data$group <- rep(paste(comparison, collapse = "-"), nrow(final_data))
+      df_plot1 = final_data
+      
+      
+      
+      #########################################################################
+      
+      group_data = multinichenet_output$prioritization_tables$group_prioritization_table_source  %>% 
+        dplyr::mutate(
+          sender_receiver = paste(sender, receiver, sep = " --> "), 
+          lr_interaction = paste(ligand, receptor, sep = " - "))  %>% 
+        dplyr::distinct(id, sender, receiver, sender_receiver, ligand, receptor, lr_interaction, group, activity_scaled, direction_regulation, prioritization_score) %>% 
+        dplyr::filter(id %in% sample_data$id) %>% 
+        dplyr::arrange(receiver) %>% 
+        dplyr::group_by(receiver) %>% 
+        dplyr::arrange(sender, .by_group = TRUE)
+      
+      df_plot2 = group_data %>% dplyr::mutate(
+        sender_receiver = factor(
+          sender_receiver, 
+          levels = group_data$sender_receiver %>% unique()
+        ))
+
+      #################################################################
+      
+      table_name <- paste(group, receiver, sender, sep = "_")
+      file_name_p1 <- file.path(plot_dir, paste0(table_name, "_LR_pairs_dfplot_median_bulk_expr.csv"))
+      write.csv(df_plot1, file_name_p1, row.names = FALSE)
+      file_name_p2 <- file.path(plot_dir, paste0(table_name, "_LR_pairs_dfplot_ligand_activity.csv"))
+      write.csv(df_plot2, file_name_p2, row.names = FALSE)
+      top_n_LR_pairs[[table_name]] <- list(df_plot1 = df_plot1, df_plot2 = df_plot2)
+      
+    }
+  }
+}
+
+
+# make plots --------------------------------------------------------------
+
+
+# TODO check if works for more groups
+for (group in groups_to_compare){ 
+  for (receiver in cell_types_selected) {
+    for (sender in cell_types_selected) {
+      
+      table_name <- paste(group, sender, receiver, sep = "_")
+      sample_data = top_n_LR_pairs[[table_name]]
+      
+      
+      df_plot1 = sample_data$df_plot1
+      df_plot2 = sample_data$df_plot2
+      
+      p1 = plot_bulk_expression(df_plot1)
+      p2 = plot_igand_activity(df_plot2)
+      
+      p = patchwork::wrap_plots(
+        p1,p2,
+        nrow = 1,guides = "collect",
+        widths = c(6,6)
+      )
+      
+      pdf(file = file.path(plot_dir,paste0("MultiNicheNet_plot_",table_name,".pdf")), width = 17, height = 10)
+      print(p)
+      dev.off()
+
+    }
+  }
+}
+
+########################
+# multinichenet_output <- readRDS(geomx_MultiNicheNet_path)
+# celltype_info <- multinichenet_output$celltype_info
+# celltype_de <- multinichenet_output$celltype_de
+# sender_receiver_info <- multinichenet_output$sender_receiver_info
+# sender_receiver_de <- multinichenet_output$sender_receiver_de
+# ligand_activities_targets_DEgenes <- multinichenet_output$ligand_activities_targets_DEgenes
+# prioritization_tables2 <- multinichenet_output$prioritization_tables
+# grouping_tbl <- multinichenet_output$grouping_tbl
+# lr_target_prior_cor <- multinichenet_output$lr_target_prior_cor
+# 
